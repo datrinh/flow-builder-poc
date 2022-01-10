4ddd3c1d59a53fca8d6f9ca6946d51d78d245737
Duc <duc@hello-charles.com>
2022-01-10 12:13:19 +0100

    WIP Viewport canvas

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,76 @@
 <script setup lang="ts">
-import * as PIXI from 'pixi.js'
-import { Viewport } from 'pixi-viewport'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
+import Canvas from './components/Canvas.vue'
 
-const canvas = ref<HTMLCanvasElement>()
+const title = ref('')
 
-onMounted(() => {
-  init()
+const dataModel = ref({
+  nodes: [
+    {
+      id: 'node1',
+      title: 'Node 1',
+      x: 0,
+      y: 0,
+    },
+    {
+      id: 'node2',
+      title: 'Node 2',
+      x: 100,
+      y: 0,
+    },
+  ],
+  links: [
+    {
+      from: 'node1',
+      to: 'node2',
+    },
+  ],
 })
 
-const init = () => {
-  const app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: 0xf3f3f3,
-  })
-  document.getElementById('canvas')?.appendChild(app.view)
-  // https://github.com/pixijs/pixijs/wiki/v5-Hacks#prevent-pinch-gesture-in-chrome
-  canvas.value?.addEventListener('wheel',
-    (e) => { e.preventDefault() },
-    { passive: false }
-  )
+const addNode = (node) => {
+  dataModel.value.nodes = [...dataModel.value.nodes, node]
+}
 
-  const viewport = new Viewport({
-    screenWidth: window.innerWidth,
-    screenHeight: window.innerHeight,
-    worldWidth: 1000,
-    worldHeight: 1000,
-    // stopPropagation: true,
-    // passiveWheel: true,
-    interaction: app.renderer.plugins.interaction, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-  })
+const onSubmit = () => {
+  addNode({ title: title.value, id: Date.now().toString(), x: 200, y: 0 })
+}
 
-  app.stage.addChild(viewport)
+const onElementClicked = (ev) => {
+  console.log('ev', ev)
+  ev.el.scale.x *= 1.25
+  ev.el.scale.y *= 1.25
+}
 
-  // activate plugins
-  viewport.drag().pinch().wheel().decelerate()
+const onCanvasClicked = (ev) => {
+  console.log('ev', ev)
+  const { clientX: x, clientY: y } = ev.data.originalEvent
+  const newNode = { title: 'randon', id: '', x, y }
+  addNode(newNode)
+}
 
-  for (let i = 0; i < 10; i++) {
-    const square = new PIXI.Sprite(PIXI.Texture.WHITE)
-    square.position.set(i * 100, (app.screen.height - 100) / 2)
-    square.width = 50
-    square.height = 50
-    square.tint = 0x00ff00
-
-    viewport.addChild(square)
-  }
+const onDrag = (ev) => {
+  console.log('ev', ev)
 }
 </script>
 
 <template>
   <div>
-    <div id="canvas" ref="canvas"></div>
-    <div class="fixed top-4 right-4 h-[50vh] w-60 bg-white rounded-sm p-4 shadow-sm">Widget</div>
+    <Canvas :data-model="dataModel" @element-clicked="onElementClicked" @canvas-clicked="onCanvasClicked"></Canvas>
+    <div class="fixed top-4 right-4 h-[50vh] w-60 bg-white rounded-sm p-4 shadow-lg overflow-auto">
+      <h1>Widget</h1>
+      <form @submit.prevent="onSubmit">
+        <input type="text" v-model="title" class="border" />
+        <button type="submit">Add</button>
+      </form>
+
+      <pre>
+        {{ dataModel }}
+      </pre>
+    </div>
+    <div
+      draggable
+      @dragstart="onDrag"
+      class="fixed bottom-0 left-4 w-60 h-60 bg-red-300 shadow-lg translate-y-40 hover:translate-y-28 transition-transform"
+    ></div>
   </div>
 </template>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import * as PIXI from 'pixi.js'
+import { Viewport } from 'pixi-viewport'
+import { onMounted, ref, watch } from 'vue'
+
+const props = defineProps({
+  dataModel: {
+    type: Object,
+    required: true,
+  },
+})
+const emit = defineEmits(['element-clicked', 'canvas-clicked'])
+
+const canvas = ref<HTMLCanvasElement>()
+let viewport: Viewport
+
+onMounted(() => {
+  init()
+})
+
+const init = () => {
+  const app = new PIXI.Application({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: 0xf3f3f3,
+  })
+  document.getElementById('canvas')?.appendChild(app.view)
+  // https://github.com/pixijs/pixijs/wiki/v5-Hacks#prevent-pinch-gesture-in-chrome
+  canvas.value?.addEventListener(
+    'wheel',
+    (e) => {
+      e.preventDefault()
+    },
+    { passive: false }
+  )
+
+  viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: 1000,
+    worldHeight: 1000,
+    interaction: app.renderer.plugins.interaction, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  })
+  console.log('app.renderer.plugins.interaction', app.renderer.plugins.interaction)
+  viewport.on('pointerdown', (ev) => {
+    emit('canvas-clicked', ev)
+  })
+
+  app.stage.addChild(viewport)
+
+  // activate plugins
+  viewport.drag().pinch().wheel().decelerate()
+
+  render()
+}
+
+const render = () => {
+  props.dataModel.nodes.forEach((node) => {
+    const square = createSquare({ x: node.x, y: node.y })
+
+    viewport.addChild(square)
+  })
+}
+
+watch(props, () => {
+  render()
+})
+
+const createSquare = (position: { x: number; y: number }) => {
+  const square = new PIXI.Sprite(PIXI.Texture.WHITE)
+  square.position.set(position.x, position.y)
+  square.width = 50
+  square.height = 50
+  square.tint = 0x00ff00
+  square.interactive = true
+  square.buttonMode = true
+  square.on('pointerdown', (ev) => {
+    emit('element-clicked', { el: square, ev })
+  })
+  square.on('pointerover', (ev) => {
+    square.scale.x *= 1.25
+    square.scale.y *= 1.25
+    // emit('element-hovered', { el: square, ev })
+  })
+  square.on('pointerout', (ev) => {
+    square.scale.x *= 0.75
+    square.scale.y *= 0.75
+    // emit('element-hovered', { el: square, ev })
+  })
+
+  return square
+}
+</script>
+
+<template>
+  <div id="canvas" ref="canvas"></div>
+</template>
