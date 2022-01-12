2bf4da429f8ec1cf3372f934daf085cb679ae7e7
Duc <duc@hello-charles.com>
2022-01-12 16:12:43 +0100

    directly create link

--- src/composables/useLinks.ts
+++ src/composables/useLinks.ts
@@ -1,6 +1,8 @@
 import { Position, useStorage } from '@vueuse/core'
 import { NodeModel } from '../types'
 import { v4 as uuid } from 'uuid'
+import CanvasLink from '../components/CanvasLink'
+import useCanvas from './useCanvas'
 
 type LinkDirection = 'unidirectional' | 'bidirectional'
 
@@ -11,11 +13,15 @@ interface Link {
 }
 
 const links = useStorage<Link[]>('links', [])
+const { viewport } = useCanvas()
 
 const useLinks = () => {
   const addLink = (from: Link['from'], to: Link['to'], direction: LinkDirection = 'unidirectional') => {
     const newLink = { from, to, direction }
     links.value = [...links.value, newLink]
+
+    const canvasEl = CanvasLink({ from, to })
+    viewport.addChild(canvasEl)
   }
 
   return {
